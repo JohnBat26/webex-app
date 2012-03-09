0a48b315fc047bae459cd99c0f7ab653c4d45004
Eugene Batogov <johnbat26@gmail.com>
2012-03-09 11:31:45 +0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebexApp::Application.config.secret_token = '3a127bb147895899231d7d4bd2a407292390eff631e3698df9814194c00d30bf695c67bf29b026dfd49c9b98c0bd9a3237b41775ccfc85911802e92e74d244ca'
